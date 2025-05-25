14b04364e30c2c43a353f9072b7f8849475686ae
Jesse-79-Crypto <jessecharlesharrington@gmail.com>
2025-05-25 09:29:09 -0700

    Create gunicorn.conf.py

---
+++ gunicorn.conf.py
@@ -0,0 +1,9 @@
+timeout = 180          # 3 minutes for blockchain confirmations
+
+keepalive = 120        # Keep connections alive
+
+max_requests = 1000    # Restart workers periodically
+
+preload_app = True     # Improve performance
+
+workers = 1            # Single worker for consistent state
