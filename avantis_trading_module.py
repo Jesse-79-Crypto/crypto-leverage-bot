4be5d736c2bd0189f0f36e8bbfe663744ca5ea4e
Jesse-79-Crypto <jessecharlesharrington@gmail.com>
2025-06-04 16:47:46 -0700

    update gas price

--- avantis_trading_module.py
+++ avantis_trading_module.py
@@ -1293,7 +1293,7 @@ class AvantisTrader:
                 ).build_transaction({
                 'from': trader_address,
                 'gas': 500000,
-                'gasPrice': web3.eth.gas_price,
+                'gasPrice': int(web3.eth.gas_price * 1.2),
                 'nonce': web3.eth.get_transaction_count(trader_address, 'pending')
                 })
 
