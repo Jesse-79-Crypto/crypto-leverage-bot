6c01ad846340449cc42a398929be33e41e6a0160
Jesse-79-Crypto <jessecharlesharrington@gmail.com>
2025-07-15 15:48:24 -0700

    Update bmx_trading_module.py

--- bmx_trading_module.py
+++ bmx_trading_module.py
@@ -72,6 +72,33 @@ USDC_ABI = [
     }
 ]
 
+ROUTER_ABI = [
+    {
+        "inputs":[{"name":"_plugin","type":"address"}],
+        "name":"approvePlugin",
+        "outputs":[],
+        "type":"function"
+    },
+    {
+        "inputs":[
+            {"name":"_path","type":"address[]"},
+            {"name":"_indexToken","type":"address"},
+            {"name":"_amountIn","type":"uint256"},
+            {"name":"_minOut","type":"uint256"},
+            {"name":"_sizeDelta","type":"uint256"},
+            {"name":"_isLong","type":"bool"},
+            {"name":"_acceptablePrice","type":"uint256"},
+            {"name":"_executionFee","type":"uint256"},
+            {"name":"_referralCode","type":"bytes32"},
+            {"name":"_callbackTarget","type":"address"}
+        ],
+        "name":"createIncreasePosition",
+        "outputs":[{"type":"bytes32"}],
+        "stateMutability":"payable",
+        "type":"function"
+    }
+]
+
 # BMX Position Router ABI (GMX v1 style with BMX modifications)
 BMX_POSITION_ROUTER_ABI = [
     {
